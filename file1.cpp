57db09fbc2889e012c00723571925cded08da407
FadhilM <mudzakifadhil@gmail.com>
2025-03-18 15:32:07 +0700

    perulangan for untuk pencacah naik

--- file1.cpp
+++ file1.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main(){
-    
+    //perulangan for untuk pencacah naik
+    int i;
+    cout << "Perulangan For Pencacah Naik" << endl;
+    for (i = 0; i <= 4; i++){
+            cout << "Nilai i : " << i
+            << " Lari keliling lapangan" << endl;
+    }
+
+    cout << "Nilai i terakhir : " << i << endl;
+    cout << endl;
+
+
 }
