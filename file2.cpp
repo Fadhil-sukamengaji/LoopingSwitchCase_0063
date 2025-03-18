b47b7014999b6c483e0cd88ad84efd249c4f8db0
FadhilM <mudzakifadhil@gmail.com>
2025-03-18 15:59:18 +0700

    perulangan while

--- file2.cpp
+++ file2.cpp
@@ -4,5 +4,21 @@
 using namespace std;
 
 int main (){
-   
+   //perulangan while
+   cout << "Perulangan While" << endl;
+   int x;
+   int perulanganWhile = 0;
+   int perulanganDoWhile = 0;
+   srand(time(0));
+   x = rand() % 10;
+
+   cout << "Nilai x awal : " << x << endl;
+   while(x < 7){
+        cout << "Nilai x awal : " << x << " Lari keliling lapangan" << endl;
+        x = rand() % 10;
+        perulanganWhile++;
+   }
+   cout << "Nilai x akhir : " << x << endl;
+   cout << "Jumlah perulangan : " << perulanganWhile << endl;
+   cout << endl;
 }
